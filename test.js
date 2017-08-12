7b1fe0fea36afe99ae90f612cc0b69712136eb12
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2017-08-11 17:06:18 -0700

    Don't bother using the extension, eslint complains

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 /* eslint-disable flowtype/require-parameter-type, flowtype/require-return-type, no-magic-numbers */
 import {test} from "tap"
 
-import ifThenElse from "./source.js"
+import hammer from "./source"
 
 const toFloat = (value) => parseFloat(value)
 const toString = (value) => `${value}`
