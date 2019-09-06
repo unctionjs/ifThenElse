370bf8185196d67e1b49b9798a3da018d51d71ac
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2019-09-06 14:56:46 -0700

    The transformer didnt fix this

--- test.ts
+++ test.ts
@@ -5,10 +5,10 @@ const toFloat = (value) => parseFloat(value);
 const toString = (value) => `${value}`;
 const isEven = (value) => value % 2 === 0;
 
-test(() => {
+test("works", () => {
   expect(ifThenElse(isEven)(toString)(toFloat)(1)).toEqual(1.0);
 });
 
-test(() => {
+test("works", () => {
   expect(ifThenElse(isEven)(toString)(toFloat)(2)).toEqual("2");
 });
